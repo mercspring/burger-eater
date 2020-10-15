d299af76261939f3a03cc6e4f6250534a85eb2c3
mercspring <68706508+mercspring@users.noreply.github.com>
2020-10-15 16:21:17 -0700

    wrote database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUE ("Hamburger");
+INSERT INTO burgers (burger_name) VALUE ("Chili Burger");
+INSERT INTO burgers (burger_name) VALUE ("Cheese Burger");
